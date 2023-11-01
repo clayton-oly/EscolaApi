e67fa7a26f667c239d78d564791f1770ea0540fc
Clayton <clayton.will08@gmail.com>
2023-11-01 16:12:10 -0300

    Deploy

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EscolaApi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configurações específicas do modelo podem ser adicionadas aqui
+            // Por exemplo, limitar o tamanho do campo Nome para 100 caracteres:
+            // modelBuilder.Entity<Aluno>()
+            //     .Property(a => a.Nome)
+            //     .HasMaxLength(100);
+        }
+    }
+}
